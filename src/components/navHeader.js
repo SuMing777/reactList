2b9870aeb1bc0a69cf802b0126d954f6d1cc5155
so2liu <so2liu@gmail.com>
2020-07-26 21:44:45 +0200

    refactor the code

--- src/components/navHeader.js
+++ src/components/navHeader.js
@@ -1,50 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NavHeader extends Component {
-
-  render(){
-    const { activeIndex, inputValue, btnActive } = this.props;
+  render() {
+    const { viewSetting, inputValue, isDesc } = this.props;
     return (
       <div className="header">
-        <button 
-          className={activeIndex === 1 ? "btn btn-active" : "btn"}
-          onClick={() => this.handleClickStyle(1)}
-        >列表视图</button>
         <button
-          className={activeIndex === 2 ? "btn btn-active" : "btn"}
-          onClick={() => this.handleClickStyle(2)}
-        >网格视图</button>
+          className={viewSetting === "list" ? "btn btn-active" : "btn"}
+          onClick={() => this.props.setView("list")}
+        >
+          列表视图
+        </button>
+        <button
+          className={viewSetting === "grid" ? "btn btn-active" : "btn"}
+          onClick={() => this.props.setView("grid")}
+        >
+          网格视图
+        </button>
         <input
           className="input"
           type="text"
           placeholder="请输入搜索内容"
           value={inputValue}
-          onChange={this.onChangeInputValue}
+          onChange={(e) => this.props.onChangeSearchWord(e.target.value)}
         />
-        <button 
-          className={btnActive === 1 ? "btn btn-active" : "btn"}
-          onClick={() => this.handleClickBtnStyle(1, false)}
-        >列表升序</button>
         <button
-          className={btnActive === 2 ? "btn btn-active" : "btn"}
-          onClick={() => this.handleClickBtnStyle(2, true)}
-        >列表降序</button>
+          className={isDesc === false ? "btn btn-active" : "btn"}
+          onClick={() => this.props.handleSetListOrder(false)}
+        >
+          列表升序
+        </button>
+        <button
+          className={isDesc === true ? "btn btn-active" : "btn"}
+          onClick={() => this.props.handleSetListOrder(true)}
+        >
+          列表降序
+        </button>
       </div>
-    )
+    );
   }
-
-  handleClickStyle = index => {
-    this.props.handleClickStyle(index);
-  }
-
-  handleClickBtnStyle = (index, boolean) => {
-    this.props.handleClickBtnStyle(index, boolean);
-  }
-
-  onChangeInputValue = value => {
-    this.props.onChangeInputValue(value.target.value);
-  }
-
 }
 
 export default NavHeader;
